bc8e826ed68b5eeb494686c59a4064213d30cc69
tihana <tihana23@gmail.com>
2024-01-07 18:44:01 +0100

    testg

---
+++ TestApp/Models/Ducani.cs
@@ -0,0 +1,36 @@
+﻿namespace TestApp.Models
+{
+    public class Ducani
+    {
+        public int IdDucana { get; set; }
+        public string NazivDucana { get; set; }
+
+        public string AdresaDucana { get; set; }
+        public decimal UkupanZbrojRacuna { get; set; }
+
+        public List<Racun> Racuni { get; set; }
+
+        public Ducani()
+        {
+            this.Racuni = new List<Racun>();
+        }
+
+       
+
+        public void DodajRacun(Racun racun)
+        {
+            Racuni.Add(racun);
+        }
+
+        
+
+        public Ducani(string nazivDucana, string adresaDucana)
+        {
+            this.NazivDucana= nazivDucana;
+            this.AdresaDucana = adresaDucana;
+
+
+        }
+      
+}
+}
